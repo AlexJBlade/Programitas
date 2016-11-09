628f82ce82df543374c2aaa818be90052e87843a
Alexis Romero González <neodomo@hotmail.com>
2016-11-09 17:27:19 -0600

    Ejercicio 2B

--- E02.cpp
+++ E02.cpp
@@ -14,17 +14,34 @@ Lista *crea(int valor)
     return lista;
 }
 
-void agrega(Lista *lista, int valor)
+int esta_vacia(Lista *lista)
 {
+    return (lista == NULL);
+}
+
+Lista *agrega(Lista *lista, int valor)
+{
+
+    Lista *auxiliar = crea(valor);
+
+    if (esta_vacia(lista))
+        return auxiliar;
+
     Lista *actual = lista;
+
     while(actual->siguiente != NULL) {
          actual = actual->siguiente;
     }
-    actual->siguiente = crea(valor);
+    actual->siguiente = auxiliar;
+
+    return lista;
 }
 
 int cuenta(Lista *lista)
 {
+    if (esta_vacia(lista))
+        return 0;
+
     int i=1;
     Lista *actual = lista;
     while(actual->siguiente != NULL) {
@@ -36,6 +53,9 @@ int cuenta(Lista *lista)
 
 int valor_en(Lista *lista, int posicion)
 {
+    if (esta_vacia(lista))
+        return -1;
+
     Lista *actual = lista;
     for(int i=0; i<posicion; i++){
         if (actual->siguiente == NULL)
@@ -45,8 +65,11 @@ int valor_en(Lista *lista, int posicion)
     return actual->valor;
 }
 
-void limpia(Lista *lista)
+Lista *limpia(Lista *lista)
 {
+    if (esta_vacia(lista))
+        return NULL;
+
     Lista *actual =lista;
     while(actual->siguiente != NULL){
         Lista *siguiente =actual->siguiente;
@@ -54,16 +77,24 @@ void limpia(Lista *lista)
         actual=siguiente;
     }
     free(actual);
+
+    return NULL;
 }
 
 int valor_inicio(Lista *lista)
 {
+    if (esta_vacia(lista))
+        return -1;
+
     Lista *actual = lista;
     return actual->valor;
 }
 
 int valor_fin(Lista *lista)
 {
+    if (esta_vacia(lista))
+        return -1;
+
     Lista *actual = lista;
 
     while(actual->siguiente != NULL) {
@@ -75,21 +106,56 @@ int valor_fin(Lista *lista)
 
 Lista *agrega_inicio(Lista *lista, int valor)
 {
-    // TODO
+    Lista *anterior = crea(valor);
+
+    if (esta_vacia(lista))
+        return anterior;
+
+    anterior->siguiente = lista;
+
+    return anterior;
 }
 
 Lista *agrega_en(Lista *lista, int valor, int posicion)
 {
-    // TODO
+    if (esta_vacia(lista))
+        return crea(valor);
+
+    if (posicion == 0)
+        return agrega_inicio(lista, valor);
+
+    Lista *auxiliar=crea(valor);
+    Lista *anterior = NULL;
+    Lista *actual = lista;
+    for(int i=0; i<posicion; i++){
+        if (actual->siguiente == NULL)
+            return lista;
+        anterior =actual;
+        actual = actual->siguiente;
+    }
+    anterior->siguiente = auxiliar;
+    auxiliar->siguiente = actual;
+    return lista;
 }
 
 Lista *borra_inicio(Lista *lista)
 {
-    // TODO
+    if (esta_vacia(lista))
+        return NULL;
+
+    Lista *anterior = lista;
+    Lista *actual = lista->siguiente;
+
+    free(anterior);
+
+    return actual;
 }
 
 Lista *borra_fin(Lista *lista)
 {
+    if (esta_vacia(lista))
+        return NULL;
+
     Lista *actual =lista;
     Lista *anterior =NULL;
 
@@ -98,7 +164,7 @@ Lista *borra_fin(Lista *lista)
         actual = actual->siguiente;
 
     }
-    anterior->siguiente=NULL;
+    anterior->siguiente = NULL;
     free(actual);
     return lista;
 
@@ -106,26 +172,83 @@ Lista *borra_fin(Lista *lista)
 
 Lista *borra_en(Lista *lista, int posicion)
 {
-    // TODO
+    if (esta_vacia(lista))
+        return NULL;
+
+    if (posicion == 0)
+        return borra_inicio(lista);
+
+    Lista *anterior = NULL;
+    Lista *actual = lista;
+    for(int i=0; i<posicion; i++){
+        if (actual->siguiente == NULL)
+            return lista;
+        anterior = actual;
+        actual = actual->siguiente;
+    }
+    anterior->siguiente = actual->siguiente;
+    free(actual);
+    return lista;
 }
 
 Lista *borra_en_valor(Lista *lista, int valor)
 {
-    // TODO
-}
+    Lista *actual =lista;
+    Lista *anterior =NULL;
 
-int esta_vacia(Lista *lista)
-{
-    return (lista == NULL);
+    while(actual->valor != valor){
+        if (actual->siguiente == NULL)
+            return lista;
+        anterior = actual;
+        actual = actual->siguiente;
+    }
+    anterior->siguiente = actual->siguiente;
+    free(actual);
+    return lista;
 }
 
 int main()
 {
-    Lista *lista = crea(1);
-    agrega(lista, 2);
-    agrega(lista, 3);
+    Lista *lista = NULL;
+    lista = agrega(lista, 1);
+    lista = agrega(lista, 2);
+    lista = agrega(lista, 4);
+
+    printf("%d\n", valor_en(lista, 0)); // 1
     printf("%d\n", valor_en(lista, 1)); // 2
-    printf("%d\n", cuenta(lista)); // 3
-    limpia(lista);
+    printf("%d\n", valor_en(lista, 2)); // 4
+    printf("%d\n\n", cuenta(lista)); // 3
+
+    lista = agrega_en(lista, 3, 2);
+    printf("%d\n", valor_en(lista, 0)); // 1
+    printf("%d\n", valor_en(lista, 1)); // 2
+    printf("%d\n", valor_en(lista, 2)); // 3
+    printf("%d\n", valor_en(lista, 3)); // 4
+    printf("%d\n\n", cuenta(lista)); // 4
+
+    lista = borra_en(lista, 1);
+    printf("%d\n", valor_en(lista, 0)); // 1
+    printf("%d\n", valor_en(lista, 1)); // 3
+    printf("%d\n", valor_en(lista, 2)); // 4
+    printf("%d\n\n", cuenta(lista)); // 3
+
+    lista = agrega_inicio(lista, 0);
+    printf("%d\n", valor_en(lista, 0)); // 0
+    printf("%d\n", valor_en(lista, 1)); // 1
+    printf("%d\n", valor_en(lista, 2)); // 3
+    printf("%d\n\n", cuenta(lista)); // 4
+
+    lista = borra_inicio(lista);
+    printf("%d\n", valor_en(lista, 0)); // 1
+    printf("%d\n", valor_en(lista, 1)); // 3
+    printf("%d\n", valor_en(lista, 2)); // 4
+    printf("%d\n\n", cuenta(lista)); // 3
+
+    lista = borra_en_valor(lista, 3);
+    printf("%d\n", valor_en(lista, 0)); // 1
+    printf("%d\n", valor_en(lista, 1)); // 4
+    printf("%d\n\n", cuenta(lista)); // 2
+
+    lista = limpia(lista);
     return 0;
 }
